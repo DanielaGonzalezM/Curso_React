66aa97b39c94d2e871866caf876e59a70c1774d4
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-04-22 21:48:11 -0400

    Sección 12: Pruebas unitarias y de integración - Hooks

--- 05-hook-app/src/test/hooks/useCounter.test.js
+++ 05-hook-app/src/test/hooks/useCounter.test.js
@@ -1,14 +1,44 @@
-import {renderHook} from '@testing-library/react-hooks';
-import { useCounter } from '../../hooks/useCounter';
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useCounter } from "../../hooks/useCounter";
 
+describe("Pruebas en useCounter", () => {
+  test("debe de retornar 100", () => {
+    const { result } = renderHook(() => useCounter(100));
+    expect(result.current.cont).toBe(100);
+    expect(typeof result.current.increment).toBe("function");
+    expect(typeof result.current.decrement).toBe("function");
+    expect(typeof result.current.reset).toBe("function");
+  });
 
-describe('Pruebas en useCounter', () => { 
-   test('debe de retornar valores por defecto', () => { 
-       const {result} = renderHook(()=>useCounter());
-       expect(result.current.cont).toBe(10);
-       expect( typeof result.current.increment).toBe('function');
-       expect( typeof result.current.decrement).toBe('function');
-       expect( typeof result.current.reset).toBe('function');
+  test("debe de incrementar el counter en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+    act(() => {
+      increment();
+    });
+    const { cont } = result.current;
+    expect(cont).toBe(101);
+  });
 
-    }) 
- })
+  test("debe de decrementar el counter en 1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+    act(() => {
+      decrement();
+    });
+    const { cont } = result.current;
+    expect(cont).toBe(99);
+  });
+
+  test("debe de decrementar el counter en 1 y dar valor 100", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement, reset } = result.current;
+    act(() => {
+      decrement();
+      reset();
+    });
+    const { cont } = result.current;
+    expect(cont).toBe(100);
+  });
+  
+});
