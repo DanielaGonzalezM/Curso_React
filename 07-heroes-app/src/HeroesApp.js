909cd4fb928436c9510d428025b920f5282c3a07
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-05-13 20:26:51 -0400

    Sección 14: HeroesApp - Single Page Application (SPA)

---
+++ 07-heroes-app/src/HeroesApp.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const HeroesApp = () => {
+  return (
+    <div>
+        <h1>Heroes App</h1>
+    </div>
+  )
+}
+
+export default HeroesApp
---
+++ 07-heroes-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HeroesApp from './HeroesApp';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <HeroesApp />
+
+);
+
