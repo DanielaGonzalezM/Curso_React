5aaf5609dc3b40cb35746f8228924402c7f8692d
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-05-14 22:52:39 -0400

    Sección 14: HeroesApp - Single Page Application (SPA)

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -2,6 +2,10 @@ import { Link, NavLink } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const handleLogout=()=>{
+        console.log('logout')
+    }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -31,14 +35,17 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
+                    <span className='nav-item nav-link text-info'>
+                        Daniela
+                    </span>
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
