da067468ad4458dcd9b1e811adbd9eb27bcf0f7c
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-05-17 19:25:03 -0400

    Sección 15: Protección de rutas

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Link, useNavigate, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
 
 
 export const Navbar = () => {
+    const {user} = useContext(AuthContext);
     const navigate = useNavigate();
     const handleLogout=()=>{
         navigate('/login',{
@@ -46,7 +49,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-info'>
-                        Daniela
+                        {user.name}
                     </span>
                     <button 
                         className="nav-item nav-link btn" 
