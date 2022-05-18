0176d8f8e38e1bca0ebe3077c58cf0cd13ae7b06
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-05-18 17:32:05 -0400

    Sección 15: Protección de rutas

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -1,64 +1,49 @@
-import { useContext } from 'react';
-import { Link, useNavigate, NavLink } from 'react-router-dom';
-import { AuthContext } from '../../auth/authContext';
-
+import { useContext } from "react";
+import { Link, useNavigate, NavLink } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
 
 export const Navbar = () => {
-    const {user} = useContext(AuthContext);
-    const navigate = useNavigate();
-    const handleLogout=()=>{
-        navigate('/login',{
-            replace:true
-          });
-    }
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
+  const { user,dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+    dispatch({
+        type: types.logout
+      });
+    navigate("/login", {
+      replace: true,
+    });
+  };
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Asociaciones
+      </Link>
 
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/marvel">
+            Marvel
+          </NavLink>
 
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
-                </div>
-            </div>
+          <NavLink className="nav-item nav-link" to="/dc">
+            DC
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/search">
+            Search
+          </NavLink>
+        </div>
+      </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-info'>
-                        {user.name}
-                    </span>
-                    <button 
-                        className="nav-item nav-link btn" 
-                        onClick={handleLogout}
-                    >
-                        Logout
-                    </button>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-info">{user.name}</span>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
