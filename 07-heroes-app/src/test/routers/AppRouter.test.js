e5f9223600e68b09428100403e3149da5eebf81b
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-06-04 12:20:32 -0400

    Sección 16: Pruebas de nuestra aplicación de Heroes

--- 07-heroes-app/src/test/routers/AppRouter.test.js
+++ 07-heroes-app/src/test/routers/AppRouter.test.js
@@ -3,13 +3,13 @@ import { mount } from "enzyme";
 import { AuthContext } from "../../auth/authContext";
 
 describe("pruebas AppRouter", () => {
-  const contextValue = {
-    user: {
-      logged: false,
-    },
-  };
-
   test("should de mostrar el login si no está autenticado", () => {
+    const contextValue = {
+      user: {
+        logged: false,
+      },
+    };
+
     const wrapper = mount(
       <AuthContext.Provider value={contextValue}>
         <AppRouter />
@@ -17,5 +17,23 @@ describe("pruebas AppRouter", () => {
     );
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find("h1").text().trim()).toBe("Login");
+  });
+
+  test("should de mostrarel componente de marvel si está auntenticadp", () => {
+    const contextValue = {
+      user: {
+        logged: true,
+        name: "Dani",
+      },
+    };
+
+    const wrapper = mount(
+      <AuthContext.Provider value={contextValue}>
+        <AppRouter />
+      </AuthContext.Provider>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(".navbar").exists()).toBeTruthy();
   });
 });
