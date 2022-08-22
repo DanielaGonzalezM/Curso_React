55bb344a75fc0399874c4fd7ce5afb413d1b61b2
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-08-22 17:53:56 -0400

    Sección 17: JournalApp  - MaterialUI - Estructura y Diseño

---
+++ 08-journal-app/src/JournalApp.jsx
@@ -0,0 +1,10 @@
+
+const JournalApp = () => {
+  return (
+    <>
+      <h1>hola mundo</h1>
+    </>
+  );
+};
+
+export default JournalApp;
---
+++ 08-journal-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import JournalApp from './JournalApp'
+import './styles.css'
+//npm run-script dev
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JournalApp />
+  </React.StrictMode>
+)
