2d4d265d05601dcb1f312168ad4da9c64b7e1848
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-08-23 18:05:37 -0400

    Sección 17: JournalApp - MaterialUI - Estructura y Diseño

--- 08-journal-app/src/JournalApp.jsx
+++ 08-journal-app/src/JournalApp.jsx
@@ -1,10 +1,11 @@
 import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
 const JournalApp = () => {
   return (
-    <>
+    <AppTheme>
       <AppRouter />
-    </>
+    </AppTheme>
   );
 };
 
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,3 +1,9 @@
+import { Typography } from "@mui/material";
+
 export const JournalPage = () => {
-  return <div>JournalPage</div>;
+  return (
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+    </>
+  );
 };
