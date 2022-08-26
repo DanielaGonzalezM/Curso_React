f6faa15bb45711cdf9dffae2e423858ad0842d71
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-08-26 18:11:06 -0400

    Sección 17: JournalApp - MaterialUI - Estructura y Diseño

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/system";
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/*Navbar drawerWidth*/}
+      <NavBar drawerWidth={drawerWidth} />
+
       {/*Sidebar drawerWidth*/}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/*Toolbarr */}
