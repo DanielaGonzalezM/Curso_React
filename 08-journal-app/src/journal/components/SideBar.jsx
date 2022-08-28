bf0fed9880ed21210fb16149ac296c68b7d60eae
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2022-08-27 22:08:38 -0400

    Sección 17: JournalApp - MaterialUI - Estructura y Diseño

---
+++ 08-journal-app/src/journal/components/SideBar.jsx
@@ -0,0 +1,62 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: `${drawerWidth}px` }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: `${drawerWidth}px`,
+          },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Daniela González
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abril"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={
+                      "efnasdnflka lsdk fsla flsk dfals flkds fklsja fdk"
+                    }
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -8,9 +9,10 @@ export const JournalLayout = ({ children }) => {
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
 
-      {/*Sidebar drawerWidth*/}
+      <SideBar drawerWidth={drawerWidth} />
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/*Toolbarr */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
