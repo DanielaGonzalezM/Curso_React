0948f1d98d2bd9e33ec53becf0345a9e32443ade
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-10-27 18:22:56 -0300

    Sección 18: Redux - RTK Query

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,7 +1,17 @@
-import { useGetTodosQuery } from "./store/apis"
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
 
 export const TodoApp = () => {
-    const { data: todos = [], isLoading } = useGetTodosQuery();
+    //const { data: todos = [], isLoading } = useGetTodosQuery();
+    const [todoId, setTodoId] = useState(1);
+    const { data: todo, isLoading } = useGetTodoQuery(todoId);
+    const nextTodo = () => {
+        setTodoId(todoId + 1)
+    }
+    const prevTodo = () => {
+        if (todoId === 1) return;
+        setTodoId(todoId - 1)
+    }
     return (
         <>
             <h1>
@@ -9,16 +19,18 @@ export const TodoApp = () => {
             </h1>
             <hr />
             <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
-            <pre>...</pre>
-            <ul>
+            <pre>{JSON.stringify(todo)}</pre>
+            <button onClick={prevTodo}>Prev Todo</button>
+            <button onClick={nextTodo}>Next Todo</button>
+            {/* <ul>
                 {todos.map(todo => (
 
                     <li key={todo.id}>
                         <strong>{todo.completed ? 'Done' : 'Pending'}</strong>{todo.title}
                     </li>
                 ))}
-            </ul>
-            <button>Next Todo</button>
+            </ul> */}
+
         </>
     )
 }
--- 09-toolkit-redux/src/store/apis/todosApi.js
+++ 09-toolkit-redux/src/store/apis/todosApi.js
@@ -8,9 +8,12 @@ export const todosApi = createApi({
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos'
+        }),
+        getTodo: builder.query({
+            query: (todoId) => `/todos/${todoId}`
         })
     })
 
 })
 
-export const { useGetTodosQuery } = todosApi;
+export const { useGetTodosQuery, useGetTodoQuery } = todosApi;
