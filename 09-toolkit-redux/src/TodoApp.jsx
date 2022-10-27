38bbe708727aeeb3940671f489e8298f01e21363
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-10-27 12:28:40 -0300

    Sección 18: Redux - RTK Query

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,13 +1,23 @@
+import { useGetTodosQuery } from "./store/apis"
 
 export const TodoApp = () => {
+    const { data: todos = [], isLoading } = useGetTodosQuery();
     return (
         <>
             <h1>
                 Todo - RTK Query
             </h1>
             <hr />
-            <h4>isLoading...</h4>
+            <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
             <pre>...</pre>
+            <ul>
+                {todos.map(todo => (
+
+                    <li key={todo.id}>
+                        <strong>{todo.completed ? 'Done' : 'Pending'}</strong>{todo.title}
+                    </li>
+                ))}
+            </ul>
             <button>Next Todo</button>
         </>
     )
--- 09-toolkit-redux/src/store/store.js
+++ 09-toolkit-redux/src/store/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { todosApi } from './apis'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon'
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemon: pokemonSlice.reducer
-  }, 
-}) 
+    pokemon: pokemonSlice.reducer,
+
+    [todosApi.reducerPath]: todosApi.reducer,
+  },
+  middleware: (getDefaultMiddleware => getDefaultMiddleware().concat(todosApi.middleware))
+})
+
+//middleware se agregó para la utilización de RTK query (todosApi)
