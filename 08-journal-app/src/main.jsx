42cf30623e9989a4e30d7b7b73eb57e91d7af71f
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-11-02 13:03:54 -0300

    Sección 19: Introducción a Redux

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import JournalApp from "./JournalApp";
+import { store } from "./store";
 import "./styles.css";
+import { Provider } from 'react-redux';
 //npm run-script dev
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
