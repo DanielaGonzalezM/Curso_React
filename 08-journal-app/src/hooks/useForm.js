dca5d4851a45e456ba9ef5a934f6946490aff6c0
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-11-10 15:27:24 -0300

    Sección 19: Introducción a Redux

--- 08-journal-app/src/hooks/useForm.js
+++ 08-journal-app/src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,10 +20,21 @@ export const useForm = (initialForm = {}, formValidations = {}) => {
     setFormState(initialForm);
   };
 
+  const createValidators = () => {
+    const formCheckedValues = {};
+    for (const formField of Object.keys(formValidations)) {
+      const [fn, errorMessage] = formValidations[formField];
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMessage;
+    }
+    setFormValidation(formCheckedValues);
+  };
   return {
     ...formState,
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation,
   };
 };
