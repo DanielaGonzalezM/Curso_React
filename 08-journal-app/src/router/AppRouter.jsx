62f8e5c319bf9e8a8613ef3e0170633304c9d391
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-11-16 19:33:28 -0300

    Sección 19: Introducción a Redux

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,21 +1,41 @@
-import { useSelector } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { async } from "@firebase/util";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { FirebaseAuth } from "../firebase/config";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { login, logout } from "../store/auth";
 import { CheckingAuth } from "../ui/components";
 
 export const AppRouter = () => {
-  const {status} = useSelector(state=>state.auth);
-  if(status === "checking"){
-    return <CheckingAuth />
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
   }
   return (
     <Routes>
-      {/* Login y registro */}
+      {status === "authenticated" ? (
+        <Route path="*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="auth/login" />} />
+      {/* // Login y registro
       <Route path="/auth/*" element={<AuthRoutes />} />
 
-      {/*JournalApp */}
-      <Route path="*" element={<JournalRoutes />} />
+      //JournalApp 
+      <Route path="*" element={<JournalRoutes />} /> */}
     </Routes>
   );
 };
--- 08-journal-app/src/ui/components/CheckingAuth.jsx
+++ 08-journal-app/src/ui/components/CheckingAuth.jsx
@@ -12,7 +12,8 @@ export const CheckingAuth = () => {
       sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
     >
       <Grid
-        item
+        container
+        direction="row"
         justifyContent="center"
       >
         <CircularProgress color="warning" />
