7b0a7760f088580b19295caacd0784e063f25fc0
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-11-17 19:34:11 -0300

    Sección 19: Introducción a Redux

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,24 +1,13 @@
-import { async } from "@firebase/util";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { FirebaseAuth } from "../firebase/config";
+
+import { useCheckAuth } from "../hooks";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { login, logout } from "../store/auth";
+
 import { CheckingAuth } from "../ui/components";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-      const { uid, email, displayName, photoURL } = user;
-      dispatch(login({ uid, email, displayName, photoURL }));
-    });
-  }, []);
+  const { status } = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
