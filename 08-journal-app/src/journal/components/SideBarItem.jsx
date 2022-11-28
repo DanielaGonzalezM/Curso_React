96369d65ade678e911c82513e620c4aab7bcc511
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2022-11-28 09:55:34 -0300

    Sección 20: Redux - CRUD

--- 08-journal-app/src/journal/components/SideBarItem.jsx
+++ 08-journal-app/src/journal/components/SideBarItem.jsx
@@ -7,14 +7,20 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title, body, id, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
+  const onClickNote = () => {
+    dispatch(setActiveNote({ title, body, id, date, imageUrls }));
+  };
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
