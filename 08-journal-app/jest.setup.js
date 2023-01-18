e041a662eb21437656379e1267263c38cba2e563
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-01-18 10:04:53 -0300

    Sección 21: Testing

--- 08-journal-app/jest.setup.js
+++ 08-journal-app/jest.setup.js
@@ -1,2 +1,3 @@
 // En caso de necesitar la implementación del FetchAPI
 import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+import 'setimmediate';
--- 08-journal-app/tests/helpers/fileUploade.test.js
+++ 08-journal-app/tests/helpers/fileUploade.test.js
@@ -1,13 +1,37 @@
-import { fileUpload } from "../../src/helpers/fileUpload"
+import { fileUpload } from "../../src/helpers/fileUpload";
+import { v2 as cloudinary } from "cloudinary";
 
-describe('Pruebas en fileUpload', () => { 
-    test('debe de subir el archivo correctamente a cloudinary', async() => { 
-        const imageURL = 'https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg';
-        const resp = await fetch(imageURL);
-        const blob = await resp.blob();
-        const file =new File([blob],'gatito.jpg');
-        const url = await fileUpload(file);
-        expect(typeof imageURL).toBe('string');
-     })
+cloudinary.config({
+  cloud_name: "cursos-dgm",
+  api_key: "555923867234813",
+  api_secret: "yNP_vLSRxYMgDmPCXV1iRRhxuIE",
+  secure: true,
+});
 
- })
+describe("Pruebas en fileUpload", () => {
+  test("debe de subir el archivo correctamente a cloudinary", async () => {
+    const imageURL =
+      "https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg";
+    const resp = await fetch(imageURL);
+    const blob = await resp.blob();
+    const file = new File([blob], "gatito.jpg");
+    const url = await fileUpload(file);
+    expect(typeof imageURL).toBe("string");
+
+    const segments = url.split("/");
+    const imageId = segments[segments.length - 1].replace(".jpg", "");
+    const cloudResp = await cloudinary.api.delete_resources(
+      ["journal/" + imageId],
+      {
+        resource_type: "image",
+      }
+    );
+    //console.log(cloudResp);
+  });
+
+  test("debe retornar null", async () => {
+    const file = new File([], "foto.jpg");
+    const url = await fileUpload(file);
+    expect(url).toBe(null);
+  });
+});
