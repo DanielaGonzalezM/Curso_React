c413f13a318159489d06522d20eae75e5e25c941
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-01-23 10:24:31 -0300

    Sección 21: Testing

--- 08-journal-app/tests/store/auth/thunks.test.js
+++ 08-journal-app/tests/store/auth/thunks.test.js
@@ -1,5 +1,10 @@
-import { checkingCredentials } from "../../../src/store/auth";
-import { checkingAuthentication } from "../../../src/store/auth/thunks";
+import { singInWithGoogle } from "../../../src/firebase/providers";
+import { checkingCredentials, login, logout } from "../../../src/store/auth";
+import {
+  checkingAuthentication,
+  startGoogleSingIn,
+} from "../../../src/store/auth/thunks";
+import { demoUser } from "../../fixtures/authFixtures";
 
 jest.mock("../../../src/firebase/providers");
 
@@ -9,5 +14,21 @@ describe("Pruebas en AuthThunks", () => {
   test("should invocar el checkingCredentials", async () => {
     await checkingAuthentication()(dispatch);
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+
+  test("should startGoogleSingIn debe llamar checkingcredentials y login exitoso", async () => {
+    const loginData = { ok: true, ...demoUser };
+    await singInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleSingIn()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test("should startGoogleSingIn debe llamar checkingcredentials y logout error", async () => {
+    const loginData = { ok: false, errorMessage: "Un error en Google" };
+    await singInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleSingIn()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
   });
 });
