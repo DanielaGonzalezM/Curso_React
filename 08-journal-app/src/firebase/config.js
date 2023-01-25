14622e2aaa28ac2709f6365489d76611b017c41b
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-01-25 10:10:16 -0300

    Sección 21: Testing

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -8,15 +8,24 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAkf8n8SLCEvMIdSAkK0BX5Sn4rXOPCR8g",
+//     authDomain: "react-curso-83e27.firebaseapp.com",
+//     projectId: "react-curso-83e27",
+//     storageBucket: "react-curso-83e27.appspot.com",
+//     messagingSenderId: "696646234604",
+//     appId: "1:696646234604:web:d6730855c10145c7c0d0c9",
+//     measurementId: "G-Y5XPQETRHK"
+// };
+//Testing only
 const firebaseConfig = {
-    apiKey: "AIzaSyAkf8n8SLCEvMIdSAkK0BX5Sn4rXOPCR8g",
-    authDomain: "react-curso-83e27.firebaseapp.com",
-    projectId: "react-curso-83e27",
-    storageBucket: "react-curso-83e27.appspot.com",
-    messagingSenderId: "696646234604",
-    appId: "1:696646234604:web:d6730855c10145c7c0d0c9",
-    measurementId: "G-Y5XPQETRHK"
-};
+    apiKey: "AIzaSyCqDnz1roQKdxNHm7JFxoktgEOlRgC3nm8",
+    authDomain: "testing-react-97b2a.firebaseapp.com",
+    projectId: "testing-react-97b2a",
+    storageBucket: "testing-react-97b2a.appspot.com",
+    messagingSenderId: "448537573114",
+    appId: "1:448537573114:web:b69d066c42b7d1d6c23b43"
+  };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
