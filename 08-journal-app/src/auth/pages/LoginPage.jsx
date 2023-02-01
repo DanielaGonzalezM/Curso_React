1f553e4b1c3fdcf50a509335f1445fc439b94027
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-01-31 21:04:46 -0300

    Sección 21: Testing

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -89,6 +89,7 @@ export const LoginPage = () => {
                 onClick={onGoogleSignIn}
                 variant="contained"
                 fullWidth
+                aria-label="google-btn"
               >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
--- 08-journal-app/tests/auth/pages/LoginPage.test.jsx
+++ 08-journal-app/tests/auth/pages/LoginPage.test.jsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import { LoginPage } from "../../../src/auth/pages/LoginPage";
 import { authSlice } from "../../../src/store/auth";
+import { notAuthenticatedState } from "../../fixtures/authFixtures";
 
 const store = configureStore({
   reducer: { auth: authSlice.reducer },
+  preloadedState: { auth: notAuthenticatedState },
 });
 describe("Pruebas LoginPage", () => {
   test("should mostrar componente correctamente", () => {
@@ -19,5 +21,18 @@ describe("Pruebas LoginPage", () => {
     );
 
     expect(screen.getAllByText("Login").length).toBeGreaterThanOrEqual(1);
+  });
+
+  test("should google debe llamar al startGoogleSingIn", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const googleBtn = screen.getByLabelText("google-btn");
+    fireEvent.click(googleBtn);
   });
 });
