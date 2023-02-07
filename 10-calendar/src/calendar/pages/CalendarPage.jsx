dc5add668f25140aa802b502e2aab589b2f1bc77
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-02-07 16:56:33 -0300

    Sección 22: MERN Calendar

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,45 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
 import { Navbar } from "../";
 
+const events = [
+  {
+    title: "Cumpleaños",
+    notes: "Comprar torta",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Daniela",
+    },
+  },
+];
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </>
   );
 };
