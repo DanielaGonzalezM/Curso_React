8ddec69bac828807cc2764afbfe6e1f01da0359e
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-02-10 11:13:44 -0300

    Sección 22: MERN Calendar

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,8 +1,11 @@
 import { addHours } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+import es from "date-fns/locale/es";
+registerLocale("es", es);
 
 const customStyles = {
   content: {
@@ -54,6 +57,9 @@ export const CalendarModal = () => {
               onDateChanged(event, "start");
             }}
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
@@ -67,6 +73,9 @@ export const CalendarModal = () => {
               onDateChanged(event, "end");
             }}
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
