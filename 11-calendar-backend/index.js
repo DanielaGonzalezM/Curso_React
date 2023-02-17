143b0beb0f8b460c25ac538dd2a1375679408d12
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-02-17 14:25:27 -0300

    Sección 23: CalendarApp - Backend - Node, Express, Mongo

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -1,1 +1,17 @@
-console.log("holiii");
+const express = require("express");
+
+//Crea el servidor de express
+const app = express();
+
+//Rutas
+app.get("/", (req, res) => {
+  console.log("test");
+  res.json({
+    ok: true
+  })
+});
+
+//Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`servidor corriendo en puerto ${4000}`);
+});
