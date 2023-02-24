12ffcce70b0dff9ba14eb2ec0bff2e23129d7ec5
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-02-23 21:18:59 -0300

    Sección 23: CalendarApp - Backend - Node, Express, Mongo

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
+const { generarJWT } = require("../helpers/jwt");
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
@@ -18,10 +19,14 @@ const crearUsuario = async (req, res = response) => {
     usuario.password = bcrypt.hashSync(password, salt);
 
     await usuario.save();
+
+    const token = await generarJWT(usuario.uid, usuario.name);
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
       msg: "registro exitoso",
     });
   } catch (error) {
@@ -52,12 +57,14 @@ const loginUsuario = async (req, res = response) => {
     }
 
     // token
+    const token = await generarJWT(usuario.uid, usuario.name);
 
     res.status(200).json({
       ok: true,
       msg: "login correcto",
       uid: usuario.id,
       name: usuario.name,
+      token,
     });
   } catch (error) {
     res.status(500).json({
