4f59428c46b791cfcb2082a0607d8f439e1b6ba5
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-06 18:37:40 -0300

    Sección 26: MERN - Calendario + Backend

--- 10-calendar/src/hooks/useAuthStore.js
+++ 10-calendar/src/hooks/useAuthStore.js
@@ -1,16 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../store/auth/authSlice";
 export const useAuthStore = () => {
   const { state, user, errorMessage } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const startLogin = async ({ email, password }) => {
+    dispatch(onChecking());
     console.log(email, password);
     try {
-      const resp = await calendarApi.post("/auth", { email, password });
-      console.log({ resp });
+      const { data } = await calendarApi.post("/auth", { email, password });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(
+        onLogin({
+          name: data.name,
+          uid: data.uid,
+        })
+      );
     } catch (error) {
-      console.log({ error });
+      dispatch(onLogout("credenciales incorrectas"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
     }
   };
   return {
--- 10-calendar/src/store/auth/authSlice.js
+++ 10-calendar/src/store/auth/authSlice.js
@@ -17,7 +17,15 @@ export const authSlice = createSlice({
       state.user = payload;
       state.errorMessage = undefined;
     },
+    onLogout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.user = {};
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state /* action */) => {
+      state.errorMessage = undefined;
+    },
   },
 });
 // Action creators are generated for each case reducer function
-export const { onChecking, onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions;
