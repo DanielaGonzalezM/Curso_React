c6f2d6cffd0e81cc014233ddde908b201491e648
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-06 20:19:56 -0300

    Sección 26: MERN - Calendario + Backend

--- 10-calendar/src/router/AppRouter.jsx
+++ 10-calendar/src/router/AppRouter.jsx
@@ -18,12 +18,16 @@ export const AppRouter = () => {
   return (
     <Routes>
       {status === "not-authenticated" ? (
-        <Route path="/auth/*" element={<LoginPage />} />
+        <>
+          <Route path="/auth/*" element={<LoginPage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
       ) : (
-        <Route path="/*" element={<CalendarPage />} />
+        <>
+          <Route path="/" element={<CalendarPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
       )}
-
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
