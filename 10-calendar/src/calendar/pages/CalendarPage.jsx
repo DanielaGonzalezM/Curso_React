7c8bb116f468f178ef02fed0b0ca012f3ae47721
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-07 16:45:34 -0300

    Sección 27: MERN CRUD - Eventos del calendario

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -9,21 +9,25 @@ import {
 } from "../";
 import { getMessagesES, localizer } from "../../helpers";
 import { useEffect, useState } from "react";
-import { useUiStore, useCalendarStore } from "../../hooks";
+import { useUiStore, useCalendarStore, useAuthStore } from "../../hooks";
 
 export const CalendarPage = () => {
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
   const [lastView, setLastView] = useState(
     localStorage.getItem("lastView") || "week"
   );
   const eventStyleGetter = (event, start, end, isSelected) => {
+    const isMyEvent =
+      user.uid === event.user._id || user.uid === event.user.uid;
     const style = {
-      backgroundColor: "#347CF7",
+      backgroundColor: isMyEvent ? "#347CF7" : "#465660",
       borderRadius: "0px",
       opacity: 0.8,
       color: "white",
     };
+
     return { style };
   };
 
