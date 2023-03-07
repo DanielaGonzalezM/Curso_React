6540aa428885d57688804a510b56c9a451797522
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-07 18:26:50 -0300

    Sección 28: Fin el MERN - Desplegarlo a producción

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { AppRouter } from "./router";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -7,7 +7,9 @@ export const CalendarApp = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
+      {/* <HashRouter> */}
         <AppRouter />
+      {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   );
--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -22,6 +22,9 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`servidor corriendo en puerto ${process.env.PORT}`);
