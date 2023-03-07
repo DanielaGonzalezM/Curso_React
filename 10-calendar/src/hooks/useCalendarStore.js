2132e351df6f619f55092c3ae28a2cc63897a0e5
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-07 16:50:25 -0300

    Sección 27: MERN CRUD - Eventos del calendario

--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -35,9 +35,14 @@ export const useCalendarStore = () => {
       Swal.fire("Error al guardar", error.response.data.msg, "error");
     }
   };
-  const startDeleteEvent = () => {
-    // Todo:llegar al backend
-    dispatch(onDeleteEvent());
+  const startDeleteEvent = async () => {
+    try {
+      await calendarApi.delete(`/events/${activeEvent.id}`);
+      dispatch(onDeleteEvent());
+    } catch (error) {
+      console.log(error);
+      Swal.fire("Error al eliminar", error.response.data.msg, "error");
+    }
   };
 
   const startLoadingEvents = async () => {
