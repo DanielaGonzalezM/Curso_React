5eec3a818964af75f3d0eea781ed2d8284cc332a
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-10 12:45:35 -0300

    Sección 29: Pruebas unitarias y de integración - MERN

--- 10-calendar/tests/store/auth/authSlice.test.js
+++ 10-calendar/tests/store/auth/authSlice.test.js
@@ -1,8 +1,50 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import {
+  authSlice,
+  clearErrorMessage,
+  onLogin,
+  onLogout,
+} from "../../../src/store/auth/authSlice";
+import { authenticatedState, initialState } from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe("Pruebas en authSlice", () => {
   test("should regresar el estado inicial", () => {
     expect(authSlice.getInitialState()).toEqual(initialState);
+  });
+  test("should realizar un login", () => {
+    const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+    expect(state).toEqual({
+      status: "authenticated",
+      user: testUserCredentials,
+      errorMessage: undefined,
+    });
+  });
+  test("should realizar un logout", () => {
+    const state = authSlice.reducer(authenticatedState, onLogout());
+    expect(state).toEqual({
+      status: "not-authenticated",
+      user: {},
+      errorMessage: undefined,
+    });
+  });
+  test("should realizar un logout", () => {
+    const errorMessage = "Credenciales inválidas";
+    const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+    expect(state).toEqual({
+      status: "not-authenticated",
+      user: {},
+      errorMessage: errorMessage,
+    });
+  });
+  test("should limpiar el mensaje de error", () => {
+    const errorMessage = "Credenciales inválidas";
+    const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+    const newState = authSlice.reducer(state, clearErrorMessage());
+    expect(newState).toEqual({
+      status: "not-authenticated",
+      user: {},
+      errorMessage: undefined,
+    });
+    expect(newState.errorMessage).toBe(undefined);
   });
 });
