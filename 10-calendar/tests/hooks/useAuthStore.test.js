b9fd6604eb65c03975bb8c8a7be39b439838eaf0
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-16 10:21:07 -0300

    Sección 29: Pruebas unitarias y de integración - MERN

--- 10-calendar/tests/hooks/useAuthStore.test.js
+++ 10-calendar/tests/hooks/useAuthStore.test.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useAuthStore } from "../../src/hooks";
 import { authSlice } from "../../src/store";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { initialState } from "../fixtures/authStates";
+import { initialState, notAuthenticatedState } from "../fixtures/authStates";
+import { testUserCredentials } from "../fixtures/testUser";
 const getMockStore = (initialState) => {
   return configureStore({
     reducer: {
@@ -21,7 +22,7 @@ describe("Pruebas en useAuthStore", () => {
         <Provider store={mockStore}>{children}</Provider>
       ),
     });
-    console.log(result);
+
     expect(result.current).toEqual({
       errorMessage: undefined,
       status: "checking",
@@ -31,5 +32,26 @@ describe("Pruebas en useAuthStore", () => {
       startLogout: expect.any(Function),
       startRegister: expect.any(Function),
     });
+  });
+
+  test("should realizar el login correctamente", async () => {
+    localStorage.clear();
+    const mockStore = getMockStore({ ...notAuthenticatedState });
+    const { result } = renderHook(() => useAuthStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    await act(async () => {
+      await result.current.startLogin({ ...testUserCredentials });
+    });
+    const { errorMessage, status, user } = result.current;
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: undefined,
+      status: "authenticated",
+      user: { name: "Tester", uid: "6408a7a16ccedcb13eba1973" },
+    });
+    expect(localStorage.getItem("token")).toEqual(expect.any(String));
+    expect(localStorage.getItem("token-init-date")).toEqual(expect.any(String));
   });
 });
