e9a0a8aa491d4bab77af324a980190802216314c
/install.sh: line 11: whiptail: command not found <daniela_nightmare@hotmail.com>
2023-03-19 12:49:03 -0300

    Sección 29: Pruebas unitarias y de integración - MERN

--- 10-calendar/tests/router/AppRouter.test.jsx
+++ 10-calendar/tests/router/AppRouter.test.jsx
@@ -1,12 +1,15 @@
 import { AppRouter } from "../../src/router/AppRouter";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
+import { MemoryRouter } from "react-router-dom";
 
 jest.mock("../../src/hooks/useAuthStore");
+jest.mock("../../src/calendar", () => ({
+  CalendarPage: () => <h1>CalendarPage</h1>,
+}));
 
 describe("Pruebas en AppRouter", () => {
   const mockCheckAuthToken = jest.fn();
-  beforeEach(() => localStorage.clear());
   beforeEach(() => jest.clearAllMocks());
   test("should mostrarla pantalla de carga y llamar checkAuthToken", () => {
     useAuthStore.mockReturnValue({
@@ -18,5 +21,36 @@ describe("Pruebas en AppRouter", () => {
     // screen.debug();
     expect(screen.getByText("Cargando...")).toBeTruthy();
     expect(mockCheckAuthToken).toHaveBeenCalled();
+  });
+
+  test("should mostrarla el login en caso de no estar autenticado", () => {
+    useAuthStore.mockReturnValue({
+      status: "not-authenticated",
+      checkAuthToken: mockCheckAuthToken,
+    });
+
+    const { container } = render(
+      <MemoryRouter initialEntries={["/testAlgo"]}>
+        <AppRouter />
+      </MemoryRouter>
+    );
+    // screen.debug();
+    expect(screen.getByText("Ingreso")).toBeTruthy();
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should mostrarla el calendario si está autenticado", () => {
+    useAuthStore.mockReturnValue({
+      status: "authenticated",
+      checkAuthToken: mockCheckAuthToken,
+    });
+
+    render(
+      <MemoryRouter>
+        <AppRouter />
+      </MemoryRouter>
+    );
+    screen.debug();
+    expect(screen.getByText("CalendarPage")).toBeTruthy();
   });
 });
